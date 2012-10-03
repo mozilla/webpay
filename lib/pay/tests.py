85cd006124a58457c59cc256ffc56209c90e8bcb
Andy McKay <amckay@mozilla.com>
2012-10-02 21:04:20 -0700

    start of lib/pay (bug 795127)

--- lib/pay/tests.py
+++ lib/pay/tests.py
@@ -1,10 +1,24 @@
+import json
+
 from django import test
 from django.core.urlresolvers import reverse
 
+import jwt
 from nose.tools import eq_
 
+from moz_inapp_pay.tests import JWTtester
 
-class TestVerify(test.TestCase):
+class TestVerify(JWTtester):
 
-    def hello(self):
-        eq_(self.client.get(reverse('verify')).status_code, 200)
+    def setUp(self):
+        self.url = '/en-US' + reverse('verify')
+
+    def test_get(self):
+        eq_(self.client.get(self.url).status_code, 405)
+
+    def test_post_bad(self):
+        eq_(self.client.post(self.url).status_code, 400)
+
+    def test_post_some_jwt(self):
+        payload = self.request()
+        eq_(self.client.post(self.url, payload).status_code, 200)
--- lib/pay/views.py
+++ lib/pay/views.py
@@ -1,5 +1,33 @@
 from django import http
+from django.conf import settings
+from django.views.decorators.http import require_POST
+
+import commonware.log
+
+from moz_inapp_pay.exc import InvalidJWT, RequestExpired
+from moz_inapp_pay.verify import verify_jwt
+from session_csrf import anonymous_csrf_exempt
+
+log = commonware.log.getLogger('w.pay')
 
 
+def error(msg, request):
+    log.error(msg, exc_info=True)
+    if settings.DEBUG:
+        raise
+    else:
+        return http.HttpResponseBadRequest()
+
+@anonymous_csrf_exempt
+@require_POST
 def verify(request):
-    return http.HttpResponse('Hello world.')
+    data = request.body
+    try:
+        res = verify_jwt(data, settings.KEY, settings.SECRET)
+    except (InvalidJWT, RequestExpired):
+        return error(u'Failed to parse JWT', error)
+    if not res:
+        print res
+        return http.HttpResponseBadRequest()
+
+    return http.HttpResponse('ok')
