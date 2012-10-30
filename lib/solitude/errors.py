fca459242f5981bc6e2fac40730aabdecea9b196
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2012-10-30 03:37:54 -0700

    make solitude api client deal with errors (bug 806424)

---
+++ lib/solitude/errors.py
@@ -0,0 +1,12 @@
+from tower import ugettext as _
+
+ERROR_STRINGS = {
+    'PIN must be exactly 4 numbers long':
+     _('PIN must be exactly 4 numbers long'),
+    'PIN may only consists of numbers':
+     _('PIN may only consists of numbers'),
+    'This field is required.':
+     _('This field is required.'),
+    'Buyer with this Uuid already exists.':
+    _('Buyer with this UUID already exists.'),
+}
