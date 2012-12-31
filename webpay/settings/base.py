01f6f881ef90663b1d3767fbdca6250ce1f3aa69
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-12-31 11:47:59 -0600

    Log celeryutils errors from the z logger

--- webpay/settings/base.py
+++ webpay/settings/base.py
@@ -119,6 +119,10 @@ HAS_SYSLOG = True  # syslog is used if HAS_SYSLOG and NOT DEBUG.
 # See settings/local.py for SYSLOG_TAG, etc
 LOGGING = dict(loggers=dict(playdoh={'level': logging.DEBUG},
                             django_browserid={'level': logging.DEBUG},
+                            # This gives us "zamboni" logging such as the
+                            # celeryutils logger.
+                            z={'level': logging.ERROR},
+                            # This gives us webpay logging.
                             w={'level': logging.DEBUG}),
                handlers={'unicode': {'class':
                                      'webpay.unicode_log.UnicodeHandler'}})
