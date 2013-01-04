8318bedeb3139740b5627d65386c65ec7c5be14d
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-01-04 10:05:45 -0600

    Fix transaction migration
    The first one failed and mysql doesn't do alter statements
    within a transaction. This commit is for fixing it on a second run.
    The next commit will fix it up correctly.

--- migrations/009-add-transaction-uuid.sql
+++ migrations/009-add-transaction-uuid.sql
@@ -1,3 +1,7 @@
+-- Remove this after it has run on dev. The first one failed and alter
+-- statements are not in transactions :(
+ALTER TABLE `notices` DROP COLUMN `transaction_uuid`;
+
 ALTER TABLE `notices` ADD COLUMN `transaction_uuid` varchar(255) NOT NULL;
-ALTER TABLE `notices` DROP FOREIGN KEY `transaction_id_refs_uuid_42dc0546`;
-ALTER TABLE `notices` DROP COLUMN `transaction`;
+-- ALTER TABLE `notices` DROP FOREIGN KEY `transaction_id_refs_uuid_42dc0546`;
+ALTER TABLE `notices` DROP COLUMN `transaction_id`;
