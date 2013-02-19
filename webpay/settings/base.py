81610d42177e34193380a81c082c5f147898a9a6
Andy McKay <amckay@mozilla.com>
2013-02-19 15:13:15 -0800

    bump django version (bug 842803)

--- webpay/settings/base.py
+++ webpay/settings/base.py
@@ -2,6 +2,8 @@ import logging.handlers
 
 from funfactory.settings_base import *
 
+ALLOWED_HOSTS = []
+
 # Name of the top-level module where you put all your apps.
 # If you did not install Playdoh with the funfactory installer script
 # you may need to edit this value. See the docs about installing from a
