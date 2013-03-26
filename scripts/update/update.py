e855807afaad0d41df850bf6f8e00076a8039cb6
Andy McKay <amckay@mozilla.com>
2013-03-26 14:31:14 -0700

    pep8 the deploy script

--- scripts/update/update.py
+++ scripts/update/update.py
@@ -19,16 +19,19 @@ def create_virtualenv(ctx):
         try:
             ctx.local("virtualenv --distribute --never-download %s" % venv)
         except BadReturnCode:
-            pass # if this is really broken, then the pip install should fail
+            pass  # if this is really broken, then the pip install should fail
 
         ctx.local("rm -f %s/lib64 && ln -s ./lib %s/lib64" % (venv, venv))
 
-        ctx.local("%s/bin/pip install --exists-action=w --no-deps --no-index --download-cache=/tmp/pip-cache -f %s -r %s/requirements/prod.txt" %
-                    (venv, settings.PYREPO, settings.SRC_DIR))
+        ctx.local("%s/bin/pip install --exists-action=w --no-deps --no-index "
+                  "--download-cache=/tmp/pip-cache -f %s -r "
+                  "%s/requirements/prod.txt" %
+                  (venv, settings.PYREPO, settings.SRC_DIR))
     finally:
         # make sure this always runs
         ctx.local("rm -f %s/lib/python2.6/no-global-site-packages.txt" % venv)
-        ctx.local("%s/bin/python /usr/bin/virtualenv --relocatable %s" % (venv, venv))
+        ctx.local("%s/bin/python /usr/bin/virtualenv --relocatable %s"
+                  % (venv, venv))
 
 @task
 def update_code(ctx, ref='origin/master'):
@@ -37,9 +40,11 @@ def update_code(ctx, ref='origin/master'):
         ctx.local("git checkout -f %s" % ref)
         ctx.local("git submodule sync")
         ctx.local("git submodule update --init --recursive")
-        # Recursively run submodule sync and update to get all the right repo URLs.
+        # Recursively run submodule sync and update to
+        # get all the right repo URLs.
         ctx.local("git submodule foreach 'git submodule sync --quiet'")
-        ctx.local("git submodule foreach 'git submodule update --init --recursive'")
+        ctx.local("git submodule foreach "
+                  "'git submodule update --init --recursive'")
 
 
 @task
@@ -53,25 +58,32 @@ def compress_assets(ctx, arg=''):
     with ctx.lcd(settings.SRC_DIR):
         ctx.local("%s manage.py compress_assets %s" % (settings.PYTHON, arg))
 
+
 @task
 def schematic(ctx):
     with ctx.lcd(settings.SRC_DIR):
         ctx.local("%s %s/bin/schematic migrations" %
                   (settings.PYTHON, settings.VIRTUAL_ENV))
 
+
 @task
 def update_info(ctx, ref='origin/master'):
     with ctx.lcd(settings.SRC_DIR):
         ctx.local("git status")
         ctx.local("git log -1")
-        ctx.local("/bin/bash -c 'source /etc/bash_completion.d/git && __git_ps1'")
-        ctx.local('git show -s {0} --pretty="format:%h" > media/git-rev.txt'.format(ref))
+        ctx.local("/bin/bash -c "
+                  "'source /etc/bash_completion.d/git && __git_ps1'")
+        ctx.local('git show -s {0} --pretty="format:%h" '
+                  '> media/git-rev.txt'.format(ref))
+
 
 @task
 def checkin_changes(ctx):
     ctx.local(settings.DEPLOY_SCRIPT)
 
-@hostgroups(settings.WEB_HOSTGROUP, remote_kwargs={'ssh_key': settings.SSH_KEY})
+
+@hostgroups(settings.WEB_HOSTGROUP,
+            remote_kwargs={'ssh_key': settings.SSH_KEY})
 def deploy_app(ctx):
     ctx.remote(settings.REMOTE_UPDATE_SCRIPT)
     if getattr(settings, 'GUNICORN', False):
@@ -81,17 +93,20 @@ def deploy_app(ctx):
         ctx.remote("/bin/touch %s/wsgi/playdoh.wsgi" % settings.REMOTE_APP)
 
 
-@hostgroups(settings.CELERY_HOSTGROUP, remote_kwargs={'ssh_key': settings.SSH_KEY})
+@hostgroups(settings.CELERY_HOSTGROUP,
+            remote_kwargs={'ssh_key': settings.SSH_KEY})
 def update_celery(ctx):
     ctx.remote(settings.REMOTE_UPDATE_SCRIPT)
     if getattr(settings, 'CELERY_SERVICE', False):
         ctx.remote("/sbin/service %s restart" % settings.CELERY_SERVICE)
+
 
 @task
 def deploy(ctx):
     checkin_changes()
     deploy_app()
     update_celery()
+
 
 @task
 def pre_update(ctx, ref=settings.UPDATE_REF):
