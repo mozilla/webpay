11283ec52cd58a5fe0e981fec2bfacb675e8fa72
Andy McKay <amckay@mozilla.com>
2013-07-02 07:19:26 -0700

    turn off whitelist in prod and stage (bug 889056)

--- webpay/settings/sites/prod/settings_base.py
+++ webpay/settings/sites/prod/settings_base.py
@@ -93,6 +93,3 @@ STATSD_HOST = private.STATSD_HOST
 STATSD_PORT = private.STATSD_PORT
 STATSD_PREFIX = private.STATSD_PREFIX
 UUID_HMAC_KEY = private.UUID_HMAC_KEY
-
-USER_WHITELIST = ['^.*@mozilla\.com$', '^.*@intertek\.com$',
-                  '^.*@t-mobile\.pl$']
