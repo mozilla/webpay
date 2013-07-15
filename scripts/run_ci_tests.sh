528a71eca598f1eaa636ad6139315597679bd514
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-07-15 18:42:00 -0500

    Put a pip log where we can find it

--- scripts/run_ci_tests.sh
+++ scripts/run_ci_tests.sh
@@ -17,7 +17,7 @@ fi
 
 source $VENV/bin/activate
 
-pip install -U --exists-action=w --no-deps -q -r requirements/test.txt
+pip --log ./pip.log install -U --exists-action=w --no-deps -q -r requirements/test.txt
 
 cat > webpay/settings/local.py <<SETTINGS
 from webpay.settings.base import *
