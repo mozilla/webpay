89e2a358ae55d0e4224e43b2db72828122db4c7e
Kumar McMillan <kumar.mcmillan@gmail.com>
2014-05-02 15:47:19 -0500

    Handle failed/cancelled transactions (bug 987843)

--- media/js/pay/wait.js
+++ media/js/pay/wait.js
@@ -55,48 +55,72 @@ require(['cli', 'settings'], function(cli, settings) {
     function poll() {
         console.log('[wait] polling ' + startUrl);
 
+        function clear() {
+            clearPoll();
+            clearTransactionTimeout();
+        }
+
         cli.showProgress();
         request = $.ajax({
             type: 'GET',
             url: startUrl,
             timeout: settings.ajax_timeout,
             success: function(data, textStatus, jqXHR) {
+
+                function trackClosePayFlow() {
+                    cli.trackWebpayEvent({'action': 'payment',
+                                          'label': 'Closing Pay Flow'});
+                }
+
+                var paymentSuccess = (cli.mozPaymentProvider.paymentSuccess ||
+                                      window.paymentSuccess);
+                var paymentFailed = (cli.mozPaymentProvider.paymentFailed ||
+                                     window.paymentFailed);
+
                 if (data.status === cli.bodyData.transStatusCompleted) {
-                    clearPoll();
-                    clearTransactionTimeout();
+                    clear();
                     if (data.url) {
                         cli.trackWebpayEvent({'action': 'payment',
                                               'label': 'Redirect To Pay Flow'});
-                        console.log('[wait] Transaction completed; redirect to ' +
+                        console.log('[wait] transaction completed; redirect to ' +
                                     data.url);
                         window.location = data.url;
                     } else {
-                        console.log('[wait] Transaction completed; closing pay flow');
-                        cli.trackWebpayEvent({'action': 'payment',
-                                              'label': 'Closing Pay Flow'});
-                        var paymentSuccess = (cli.mozPaymentProvider.paymentSuccess ||
-                                              window.paymentSuccess);
+                        console.log('[wait] transaction completed; closing pay flow');
+                        trackClosePayFlow();
                         paymentSuccess();
                     }
+                } else if (data.status === cli.bodyData.transStatusFailed) {
+                    clear();
+                    cli.hideProgress();
+                    console.log('[wait] transaction failed');
+                    cli.showFullScreenError({
+                        hideConfirm: true,  // Don't allow retries.
+                        errorDetail: cli.bodyData.transFailedMsg
+                    });
+                } else if (data.status === cli.bodyData.transStatusCancelled) {
+                    clear();
+                    console.log('[wait] payment cancelled by user; closing pay flow');
+                    trackClosePayFlow();
+                    paymentFailed(cli.bodyData.cancelCode);
                 } else {
-                    // The transaction is pending or it failed.
-                    // TODO(Kumar) check for failed transactions here.
+                    // The transaction is in some kind of pending or
+                    // incomplete state.
                     console.log('[wait] transaction status: ' + data.status);
                     pollTimeout = window.setTimeout(poll, settings.poll_interval);
                 }
             },
             error: function(xhr, textStatus) {
                 if (textStatus == 'timeout') {
-                    clearPoll();
-                    clearTransactionTimeout();
-                    console.log('[pay] transaction request timed out');
+                    clear()
+                    console.log('[wait] transaction request timed out');
                     cli.trackWebpayEvent({'action': 'payment',
                                           'label': 'Transaction Request Timed Out'});
                     cli.hideProgress();
                     cli.showFullScreenError({callback: poll,
                                              errorCode: 'INTERNAL_TIMEOUT'});
                 } else {
-                    console.log('error checking transaction');
+                    console.log('[wait] error checking transaction');
                     cli.trackWebpayEvent({'action': 'payment',
                                           'label': 'Error Checking Transaction'});
                     pollTimeout = window.setTimeout(poll, settings.poll_interval);
