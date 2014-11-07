4332e7420ad5fabf7648ad1078a39c3d52fea6f4
Allen Short <ashort@mozilla.com>
2014-11-07 12:38:06 -0600

    turn Firefox Accounts on for production

--- webpay/settings/sites/prod/settings_base.py
+++ webpay/settings/sites/prod/settings_base.py
@@ -127,7 +127,7 @@ SPA_ENABLE_URLS = True
 SPA_SETTINGS = base.SPA_SETTINGS
 SPA_SETTINGS['ua_tracking_enabled'] = True
 
-USE_FXA = False
+USE_FXA = True
 FXA_OAUTH_URL = 'https://oauth.accounts.firefox.com'
 FXA_CLIENT_ID = private.FXA_CLIENT_ID
 FXA_CLIENT_SECRET = private.FXA_CLIENT_SECRET
