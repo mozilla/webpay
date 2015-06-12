3a347cf0de8441e409a47a73a2b6230d42d5bc89
Wil Clouser <clouserw@micropipes.com>
2015-06-12 12:11:13 -0700

    process sr_Latn automatically; bug 1168488

--- locale/omg_new_l10n.sh
+++ locale/omg_new_l10n.sh
@@ -87,6 +87,12 @@ if confirm "Process your debug language?"; then
     podebug --rewrite=unicode locale/templates/LC_MESSAGES/messages.pot locale/dbg/LC_MESSAGES/messages.po
 fi
 
+if confirm "Convert Cyrillic Serbian to Latin?"; then
+    pushd locale > /dev/null
+    msgfilter -i sr/LC_MESSAGES/messages.po -o sr_Latn/LC_MESSAGES/messages.po recode-sr-latin
+    popd > /dev/null
+fi
+
 if [ -z "$(git status --porcelain)" ]; then
     echo "Looks like there are no new strings to commit."
     exit 0
